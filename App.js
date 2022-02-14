441ff8bce2dbb59a2b8f88e29cf0aff25f05897e
JeremyJS <20186197@itla.edu.do>
2022-02-13 23:32:34 -0400

    node express.js server added

---
+++ App.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000 || process.env.PORT;
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile('/index.html')
+});
+
+app.listen(port, () => {
+});
