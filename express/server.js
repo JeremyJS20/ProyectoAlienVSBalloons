e4e65d7ccc8bc63a213fd4918b4c286450071edc
jsolano <jsolano@camsoft.com.do>
2023-02-05 23:58:12 -0400

    changes to deploy in Netlify

--- express/server.js
+++ express/server.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const serverless = require('serverless-http');
 
 app.use(express.static(__dirname));
+app.use(express.static('./node_modules/phaser/dist'));
 
 router.get('/', (req, res) => {
     res.sendFile('/index.html')
